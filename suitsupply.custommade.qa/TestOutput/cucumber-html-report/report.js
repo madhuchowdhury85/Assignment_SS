3dbb9edebc675659c5536de9ddf70092ae213df6
madhurima85 <madhu.chowdhury85@gmail.com>
2018-08-23 09:38:56 +0200

    Signed-off-by: madhurima85 <madhu.chowdhury85@gmail.com>

---
+++ suitsupply.custommade.qa/TestOutput/cucumber-html-report/report.js
@@ -0,0 +1,149 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("./Feature/SuitSupplyCustomMade.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Custom Made product create and verify",
+  "description": "",
+  "id": "custom-made-product-create-and-verify",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 5401876506,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Create a custom made Jacket and verify the product details",
+  "description": "",
+  "id": "custom-made-product-create-and-verify;create-a-custom-made-jacket-and-verify-the-product-details",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "User is in home page and verify the title of home page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "Navigate to a product and user is in custom made product detail page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "Choose the color of the product",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 7,
+  "name": "Choose the design of the product",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "Choose the product from color and design combination",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "Choose the fit of the product",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "Choose the size of the product",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "Verify the product fabric in Custom made section",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 12,
+  "name": "Verify the product fit in Custom made section",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "Verify the product size in Custom made section",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "CustomMadeProductsSteps.user_is_in_home_page_and_verify_the_title_of_home_page()"
+});
+formatter.result({
+  "duration": 268721920,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CustomMadeProductsSteps.navigate_to_a_product_and_user_is_in_custom_made_product_detail_page()"
+});
+formatter.result({
+  "duration": 4857704914,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CustomMadeProductsSteps.choose_the_color_of_the_product()"
+});
+formatter.result({
+  "duration": 3454844430,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CustomMadeProductsSteps.choose_the_design_of_the_product()"
+});
+formatter.result({
+  "duration": 1558104358,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CustomMadeProductsSteps.choose_the_product_from_color_and_design_combination()"
+});
+formatter.result({
+  "duration": 2219544916,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CustomMadeProductsSteps.choose_the_fit_of_the_product()"
+});
+formatter.result({
+  "duration": 3191223263,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CustomMadeProductsSteps.choose_the_size_of_the_product()"
+});
+formatter.result({
+  "duration": 1554619279,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CustomMadeProductsSteps.verify_the_product_fabric_in_Custom_made_section()"
+});
+formatter.result({
+  "duration": 110387161,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CustomMadeProductsSteps.verify_the_product_fit_in_Custom_made_section()"
+});
+formatter.result({
+  "duration": 58047910,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CustomMadeProductsSteps.verify_the_product_size_in_Custom_made_section()"
+});
+formatter.result({
+  "duration": 72633138,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 562051598,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 648695663,
+  "status": "passed"
+});
+});
